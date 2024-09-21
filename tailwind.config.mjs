0194d511724b0cd3e8b34f25da0b444fff36ff50
micmalti <michaelgauci.mt@gmail.com>
2024-09-21 11:26:44 +0200

    Reorg codebase

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,27 @@
 /** @type {import("tailwindcss").Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    // mode: "jit",
-	theme: {
-        extend: {
-            fontFamily: {
-                serif: ["DM Serif Display", ...defaultTheme.fontFamily.serif],
-                sans: ["Poppins", ...defaultTheme.fontFamily.sans]
-            },
-            gridTemplateColumns: {
-                '24': 'repeat(24, minmax(0, 1fr))',
-            },
-            colors: {
-                primary: "#222224",
-                secondary: "#F3F4F6",
-                tertiary: "#F9AB00"
-            },
-            transitionProperty: {
-                'opacity': 'opacity',
-            },
-        },
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  // mode: "jit",
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["DM Serif Display", ...defaultTheme.fontFamily.serif],
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      gridTemplateColumns: {
+        24: "repeat(24, minmax(0, 1fr))",
+      },
+      colors: {
+        primary: "#222224",
+        secondary: "#F3F4F6",
+        tertiary: "#F9AB00",
+      },
+      transitionProperty: {
+        opacity: "opacity",
+      },
+    },
+  },
+  plugins: [],
+};
