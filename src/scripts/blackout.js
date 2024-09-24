9bfefa787228553a1d75d32af523ace2ee46d7d0
micmalti <michaelgauci.mt@gmail.com>
2024-09-24 13:22:52 +0200

    Add fade to black animation

--- src/scripts/blackout.js
+++ src/scripts/blackout.js
@@ -1,27 +1,34 @@
-gsap.registerPlugin(ScrollTrigger);
+document.addEventListener('DOMContentLoaded', (event) => {
+  gsap.registerPlugin(ScrollTrigger);
 
-let homeSection = document.querySelector("#home");
+  let homeSection = document.querySelector("#home");
 
-if (homeSection) {
-  gsap.defaults({overwrite: 'auto', duration: 1});
-  gsap.set("body", {height: "100%"});
+  if (homeSection) {
+    gsap.defaults({overwrite: 'auto', duration: 1});
+    gsap.set("body", {height: "100%"});
 
-  var tl = gsap.timeline({
-    scrollTrigger: {
-      start: () => -0.5 * innerHeight,
-      end: () => 1.5 * innerHeight,
-      scrub: true,
-      markers: true,
-      onToggle: self => self.isActive && setSection(homeSection)
+    var tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: homeSection,
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        markers: false,
+        pin: true,
+        onToggle: self => self.isActive && setSection(homeSection),
+        onLeave: () => {
+          gsap.to(homeSection, { autoAlpha: 0, duration: 0.5 });
+        },
+      }
+    });
+
+    const contents = q(".md\\:items-centerflex"); // selector may need to be adjusted based on HTML structure
+    tl.to(contents, { autoAlpha: 0, duration: 2 }, 0);
+    const q = gsap.utils.selector(homeSection);
+
+    function setSection(newSection) {
+      var tl = gsap.timeline();
+      tl.to(newSection, { autoAlpha: 1, duration: 0.5 });
     }
-  });
-
-  const q = gsap.utils.selector(homeSection);
-
-  function setSection(newSection) {
-    var tl = gsap.timeline();
-    tl.to("h1", { y: -30, autoAlpha: 0, duration: 0.3 });
-    tl.to(newSection, { autoAlpha: 1, duration: 0.5 });
-    tl.to("h1", { y: -30, autoAlpha: 1, duration: 0.3 });
   }
-}
+});
