aa6ad80ed2b1f2ab17255c5584c7b8301b177ddf
micmalti <michaelgauci.mt@gmail.com>
2024-09-27 23:48:58 +0200

    Enable JS for GitHub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://michaelgauci.com",
-  base: '/portfolio/',
   integrations: [react(), tailwind({
     applyBaseStyles: true
   }), icon()],
