8aa70a90f72d4554ac565fed3de66c256db87408
micmalti <michaelgauci.mt@gmail.com>
2024-09-29 08:38:05 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://michaelgauci.com",
   integrations: [react(), tailwind({
     applyBaseStyles: true
-  }), icon()],
+  }), icon(), sitemap()],
   server: {
     port: 3000
   }
