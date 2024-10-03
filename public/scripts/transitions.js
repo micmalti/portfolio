e06f89462217706f45f40119736eaff114dcaac3
micmalti <michaelgauci.mt@gmail.com>
2024-10-03 16:38:53 +0200

    WIP: Create animations

--- public/scripts/transitions.js
+++ public/scripts/transitions.js
@@ -1,2 +1,31 @@
 // Text: https://thinkingbox.com/
 // Timeline: https://www.rcco.uk/about-us
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+  window.onload = function() {
+    ScrollTrigger.create({
+      trigger: "#about",
+      start: "top 30%",
+      onEnter: () => {
+        gsap.from("#tagline", {
+          opacity: 0,
+          duration: 1,
+          ease: "power2.out",
+        });
+        gsap.to("#highlight", {
+          backgroundSize: "100% 100%",
+          delay: 1,
+          duration: 1,
+          ease: "power2.inOut",
+        });
+        gsap.to("#hint", {
+          opacity: 1,
+          duration: 1,
+          delay: 2.5,
+          ease: "power2.out",
+        });
+      }
+    });
+  };
+})
