68c15a7ef94725b9d7a450473f702f99eae669e5
micmalti <michaelgauci.mt@gmail.com>
2024-10-08 17:47:50 +0200

    Add smooth scrolling

--- public/scripts/animations.js
+++ public/scripts/animations.js
@@ -3,34 +3,34 @@
 // Smooth scrolling: https://productiveshop.com/padding-vs-margin/
 // Show/hide contact button
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  gsap.registerPlugin(ScrollTrigger);
-  window.onload = function() {
-    ScrollTrigger.create({
-      trigger: "#about",
-      start: "top 30%",
-      onEnter: () => {
-        gsap.from("#tagline", {
-          opacity: 0,
-          duration: 1,
-          ease: "power2.out",
-        });
-        gsap.to("#highlight", {
-          backgroundSize: "100% 100%",
-          delay: 1,
-          duration: 1,
-          ease: "power2.inOut",
-        });
-        gsap.to("#hint", {
-          opacity: 1,
-          duration: 1,
-          delay: 2.5,
-          ease: "power2.out",
-        });
-      }
-    });
-  };
-})
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   gsap.registerPlugin(ScrollTrigger);
+//   window.onload = function() {
+//     ScrollTrigger.create({
+//       trigger: "#about",
+//       start: "top 30%",
+//       onEnter: () => {
+//         gsap.from("#tagline", {
+//           opacity: 0,
+//           duration: 1,
+//           ease: "power2.out",
+//         });
+//         gsap.to("#highlight", {
+//           backgroundSize: "100% 100%",
+//           delay: 1,
+//           duration: 1,
+//           ease: "power2.inOut",
+//         });
+//         gsap.to("#hint", {
+//           opacity: 1,
+//           duration: 1,
+//           delay: 2.5,
+//           ease: "power2.out",
+//         });
+//       }
+//     });
+//   };
+// })
 
 document.addEventListener('DOMContentLoaded', function() {
   let contactSection = document.getElementById('contact');
