9d337dcf4bf82be077f523a8ccd26627eac32c7f
micmalti <michaelgauci.mt@gmail.com>
2024-10-14 14:18:09 +0200

    WIP

--- public/scripts/Tagline.js
+++ public/scripts/Tagline.js
@@ -2,9 +2,9 @@ const tagline = new SplitType('#tagline')
 
 gsap.from('.char', {
   yPercent: 130,
-  stagger: 0.02,
+  stagger: 0.05,
   delay: 0.2,
-  duration: 0.1,
+  duration: 1,
   scrollTrigger: {
     trigger: '#tagline',
     start: 'top 80%'
