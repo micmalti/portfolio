d75c35521490c65d7f5208986db67f042784a412
micmalti <michaelgauci.mt@gmail.com>
2024-10-14 16:32:40 +0200

    WIP

--- public/scripts/Blackout.js
+++ public/scripts/Blackout.js
@@ -3,5 +3,38 @@ window.addEventListener('load', function() {
 
   gsap.to("#title", { duration:1.5, opacity:1 })
   gsap.to("#subtitle", { duration:1.5, opacity:1, delay:1})
-  gsap.to("#arrow", { duration:1, opacity:1, delay:4})
+  gsap.to("#arrow", { duration:2, opacity:1, delay:4})
+
+//   gsap.to("#title", {
+//     scrollTrigger: {
+//         trigger: "#title",
+//         start: "top center",
+//         end: "bottom top",
+//         scrub: true
+//     },
+//     duration: 1,
+//     opacity: 0
+//   });
+
+//   gsap.to("#subtitle", {
+//       scrollTrigger: {
+//           trigger: "#subtitle",
+//           start: "top center",
+//           end: "bottom top",
+//           scrub: true
+//       },
+//       duration: 1,
+//       opacity: 0
+//   });
+
+//   gsap.to("#arrow", {
+//     scrollTrigger: {
+//         trigger: "#subtitle",
+//         start: "top 0",
+//         scrub: true
+//     },
+//     duration: 1,
+//     opacity: 0
+// });
+
 });
--- public/scripts/Tagline.js
+++ public/scripts/Tagline.js
@@ -1,13 +1,13 @@
-const tagline = new SplitType('#tagline')
+// const tagline = new SplitType('#tagline')
 
-gsap.from('.char', {
-  yPercent: 130,
-  stagger: 0.05,
-  delay: 0.2,
-  duration: 1,
-  scrollTrigger: {
-    trigger: '#tagline',
-    start: 'top 80%'
-  }
+// gsap.from('.char', {
+//   yPercent: 130,
+//   stagger: 0.05,
+//   delay: 0.2,
+//   duration: 1,
+//   scrollTrigger: {
+//     trigger: '#tagline',
+//     start: 'top 80%'
+//   }
 
-})
+// })
