345275efabafc4c3b4f9a0117c8f8770aaa5a234
micmalti <michaelgauci.mt@gmail.com>
2025-06-19 11:23:00 +0200

    Configure Shiki for code block rendering
    Closed #67

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,21 @@ import icon from "astro-icon";
 import mdx from '@astrojs/mdx';
 import sitemap from "@astrojs/sitemap";
 import lottie from "astro-integration-lottie";
+import expressiveCode from "astro-expressive-code";
 
 export default defineConfig({
   site: "https://michaelgauci.com",
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: true,
-    }),
-    icon(),
-    sitemap(),
-    lottie(),
-    mdx()
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: true,
+  }), icon(), sitemap(), lottie(), expressiveCode({
+    themes: ['github-light'],
+    useThemedScrollbars: true,
+    styleOverrides: {
+      // frames: {
+        // shadowColor: '#f9f9f9',
+      // },
+    },
+  }), mdx()],
   server: {
     port: 3000,
   },
